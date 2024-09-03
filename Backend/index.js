27f98a065a341fd358fefbd77202554a6a8c58b8
Chanchal Sen <92750541+ChanchalSen09@users.noreply.github.com>
2024-09-04 01:43:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,13 +15,17 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
-app.get("/", (req, res) => {
-  res.send("Helllo");
+app.get('*', (req, res) => {
+  res.send('Welcome to the Quiz App Backend!');
 });
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "An internal server error occurred." });
 });
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
